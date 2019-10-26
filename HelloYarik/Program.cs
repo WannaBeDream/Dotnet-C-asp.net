a303f45d17f3b03c106285da92d20a079cbd32cc
Alex <alexxldub@gmail.com>
2019-10-26 17:01:49 +0300

    Добавьте файлы проекта.

---
+++ HelloYarik/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            Console.Title = "Бля взломали демоны";
+            string hello = "Привет Ярик,да я тебя взломал";
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.WriteLine(hello);
+                key = Console.ReadKey(true);
+
+                if ((key.Modifiers & ConsoleModifiers.Control) != 0)
+                    Console.WriteLine("Добро пожаловать в жестокий мир С# клавиша ctrl удалена");
+                if ((key.Modifiers & ConsoleModifiers.Alt) != 0)
+                    Console.WriteLine("Добро пожаловать в жестокий мир С# клавиша ALT удалена");
+                Console.WriteLine(key.Key.ToString() + " НЕ ПОМОЖЕТ ");
+            } while (key.Key != ConsoleKey.Escape);
+        }
+    }
+}
